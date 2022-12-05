087fa729b867853a2b44c09acc6a104d77f3256d
j1sk1ss <fotikolay222@gmail.com>
2022-12-05 17:32:54 +0500

    UI completed

---
+++ UI_ForeignLanguageTeacher.EXMPL/Windows/Editor.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace UI_ForeignLanguageTeacher.EXMPL.Windows
+{
+    public partial class Editor : Window
+    {
+        public Editor()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ UI_ForeignLanguageTeacher.EXMPL/Windows/Self-Trainer.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace UI_ForeignLanguageTeacher.EXMPL.Windows
+{
+    public partial class Self_Trainer : Window
+    {
+        public Self_Trainer()
+        {
+            InitializeComponent();
+        }
+    }
+}
